4b2dabd2a2f81d89bf752328be2ac2188664066f
LuisM <muislarques@gmail.com>
2025-07-19 14:21:16 +0100

    Add project files.

---
+++ StoreWebApp/Controllers/PaymnetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreWebApp.Controllers
+{
+    public class PaymnetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
